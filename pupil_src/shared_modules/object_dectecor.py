af3598fc347a1bdd492e20a8180dbd5f67827e4c
exponentialR <sadebayo01@qub.ac.uk>
2022-01-17 10:41:46 +0000

    object_dectecor.py and pupil yolov5

--- pupil_src/shared_modules/object_dectecor.py
+++ pupil_src/shared_modules/object_dectecor.py
@@ -28,11 +28,18 @@ device = torch.device('cuda')
 path = r'/home/iamshri/PycharmProjects/intent/yolov5/best.pt'
 model = torch.hub.load('ultralytics/yolov5', 'custom', path)
 model.to(device)
+names = model.names
+empty_array = np.zeros(16)
 
 from plugin import Plugin
 import logging
 
 logger = logging.getLogger((__name__))
+
+
+def extract_fixations(occurrence):
+    return np.array([pt["norm_pos"] for pt in occurrence.get(
+        "fixations")]).flatten() if "fixations" in occurrence else np.zeros(2 * 2)
 
 
 class ObjectDetector(Plugin):
@@ -49,14 +56,15 @@ class ObjectDetector(Plugin):
         self.menu = None
         self.img = None
         self.new_window = False
-        self.f_content = json.load(f)
+        # self.f_content = json.load(f)
         # self.root_folder = self.f_content['Data_Subfolder']
-        self.root_folder = os.path.join(ROOT_FOLDER, self.f_content['Data_Directory'])
+        # self.root_folder = os.path.join(ROOT_FOLDER, self.f_content['Data_Directory'])
         self.frame_num = 1
         self.current_action = 0
         self.start_time = None
         self.passed_time = []
         self.running = True
+        self.rect_array = np.zeros(16)
         self.hands = mp.solutions.hands
         self.mp_drawing = mp.solutions.drawing_utils
         # self.mp_drawing_styles = mp.solutions.drawing_styles
@@ -75,12 +83,6 @@ class ObjectDetector(Plugin):
     def draw_landmarks(self, image, results):
         for hand_landmarks in results.multi_hand_landmarks:
             self.mp_drawing.draw_landmarks(image, hand_landmarks, self.hands.HAND_CONNECTIONS)
-
-    # def leftorright(self, image, coords):
-    #     for idx, hand_landmarks in enumerate(coords.multi_hand_landmarks):
-    #         self.mp_drawing.draw_landmarks(image, hand_landmarks, self.hands.HAND_CONNECTIONS, self.mp_drawing_styles.get_default_hand_landmarks_style(), self.mp_drawing_styles.get_default_hand_connections_style)
-    #     for _, classification in enumerate(coords.multi_handedness):
-    #         if classification.classification[0].index == idx and classification
 
     @staticmethod
     def extract_coordinates(results, occurrence):
@@ -101,31 +103,82 @@ class ObjectDetector(Plugin):
                     # fixation_pts = np.array([pt["norm_pos"] for pt in events.get("fixations")]).flatten() if "fixations" in events else np.zeros(2*2)
                     return np.concatenate([fixation_pts, right_hand, left_hand])
 
-    def extract_fixations(self, occurrence):
-        return np.array([pt["norm_pos"] for pt in occurrence.get(
-            "fixations")]).flatten() if "fixations" in occurrence else np.zeros(2 * 2)
-
     def recent_events(self, events):
         if not self.running or "frame" not in events:
             return
         frame = events['frame'].img
-        detect_obj = model(frame[..., ::-1])
-        results = detect_obj.pandas().xyxy[0].to_dict(orient='records')
-        print(results)
-            # if results.multi_hand_landmarks:
-            # for idx, hand_coordinates in enumerate(results.multi_hand_landmarks):
-            #     for _, classification in enumerate(results.multi_handedness):
-            #         right_hand = np.array([[coordinates_.x, coordinates_.y, coordinates_.z] for coordinates_ in
-            #                                hand_coordinates.landmark]).flatten() if classification.classification[
-            #                                                                             0].index == 1 else np.zeros(
-            #             21 * 3)
-            #         left_hand = np.array([[coordinates_.x, coordinates_.y, coordinates_.z] for coordinates_ in
-            #                               hand_coordinates.landmark]).flatten() if classification.classification[
-            #                                                                            0].index == idx else np.zeros(
-            #             21 * 3)
-        for results_ in results:
-            results__ = np.array([[int(results_['xmin']), int(results_['ymin']), int(results_['xmax']), int(results_['ymax'])] for results_ in results]).flatten()  if results_['class'] == 1 else np.zeros(4)
-            rec_coords = np.array(results__)
-            print(rec_coords)
-            x1, y1, x2, y2 = rec_coords[0], rec_coords[1], rec_coords[2], rec_coords[3]
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 255), 2)
+        image = frame.copy()
+        detect_obj = model(image[..., ::-1])
+        results = detect_obj.pandas().xywhn[0].to_dict(orient='records')
+        rect_bbox = detect_obj.pandas().xyxy[0].to_dict(orient='records')
+        # print(results)
+
+        for i in results:
+            if i['class'] == 0:  # Book
+                empty_array[0] = i['xcenter']
+                empty_array[1] = i['ycenter']
+                empty_array[2] = i['width']
+                empty_array[3] = i['height']
+
+            elif i['class'] == 1:  # Mug
+                empty_array[0] = i['xcenter']
+                empty_array[1] = i['ycenter']
+                empty_array[2] = i['width']
+                empty_array[3] = i['height']
+
+            elif i['class'] == 2:  # Mugs
+                empty_array[4] = i['xcenter']
+                empty_array[5] = i['ycenter']
+                empty_array[6] = i['width']
+                empty_array[7] = i['height']
+
+            else:  # Stacked_Books
+                empty_array[8] = i['xcenter']
+                empty_array[9] = i['ycenter']
+                empty_array[10] = i['width']
+                empty_array[11] = i['height']
+            # print(names[1])
+        # print(empty_array)
+        for res in rect_bbox: #plot bounding boxes and include labels
+            if res['class'] == 0:  # Book
+                l = int(res['xmin'])
+                t = int(res['ymin'])
+                r = int(res['xmax'])
+                b = int(res['ymax'])
+                text_in_image = res['name']
+                cv2.rectangle(frame, (l, t), (r, b), (0, 0, 255), 1)
+                cv2.putText(frame, text_in_image, (l, t), cv2.FONT_HERSHEY_DUPLEX, 0.35, (255, 100, 120), 1,
+                            cv2.LINE_AA)
+
+            elif res['class'] == 1:  # Mug
+                l1 = int(res['xmin'])
+                t1 = int(res['ymin'])
+                r1 = int(res['xmax'])
+                b1 = int(res['ymax'])
+                text_in_image = res['name']
+                # cv2.rectangle(frame, (l1, t1), (r1, b1), (255, 0, 0), 1)
+                # cv2.putText(frame, text_in_image, (l1, t1), cv2.FONT_HERSHEY_DUPLEX, 0.35, (255, 100, 120), 1,
+                #             cv2.LINE_AA)
+
+            elif res['class'] == 2:  # Mugs
+                l2 = int(res['xmin'])
+                t2 = int(res['ymin'])
+                r2 = int(res['xmax'])
+                b2 = int(res['ymax'])
+                text_in_image = res['name']
+                # cv2.rectangle(frame, (l2, t2), (r2, b2), (255, 0, 255), 1)
+                # cv2.putText(frame, text_in_image, (l2, t2), cv2.FONT_HERSHEY_DUPLEX, 0.35, (255, 100, 120), 1,
+                #             cv2.LINE_AA)
+
+            else:
+                l3 = int(res['xmin'])
+                t3 = int(res['ymin'])
+                r3 = int(res['xmax'])
+                b3 = int(res['ymax'])
+                text_in_image = res['name']
+                cv2.rectangle(frame, (l3, t3), (r3, b3), (0, 255, 0), 1)
+                cv2.putText(frame, text_in_image, (l3, t3), cv2.FONT_HERSHEY_DUPLEX, 0.35, (255, 100, 120), 1,
+                            cv2.LINE_AA)
+
+
+
